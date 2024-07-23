d6931aa0bafa3f18c1ab50f14f4d84664ac23285
Luke Bennett <luke.bennett@mdrx.tech>
2024-07-23 12:32:57 +0100

    fix: config syntax

--- cloud/ui/src/app/layout.tsx
+++ cloud/ui/src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { config } from "../config";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Envy",
-  description:
-    ".env for teams - Stop chasing the loose ends of sensitive values",
+  title: config.brand.name,
+  description: config.brand.tagline,
 };
 
 export default function RootLayout({
--- cloud/ui/src/config.ts
+++ cloud/ui/src/config.ts
@@ -1,9 +1,6 @@
 export const config = {
   brand: {
     name: "Envy",
-    tagline: ""
-
-    ".env for teams - Stop chasing the loose ends of sensitive values",
-  }
-
-}
+    tagline: ".env for teams - Stop chasing the loose ends of sensitive values",
+  },
+};
